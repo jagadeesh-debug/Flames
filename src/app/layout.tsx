8035aac0d834313ba9f495d36092af5d378c7150
Jagadeesh <118063840+jagadeesh-debug@users.noreply.github.com>
2025-09-17 19:46:11 +0530

    Add meta tags for SEO in layout component
    Added meta tags for description and keywords.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from 'next/head';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+       <Head>
+        <meta name="description" content="falme your relationsship" />
+        <meta name="keywords" content="flame,flames,falmess" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
